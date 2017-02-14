fdbdac7d299f0e7f714f4d7d0d3c88ed925727ff
Sahil Parekh <sp5714@cloud-vm-45-211>
2017-02-14 19:47:13 +0000

    refactored, added map

--- system1/process.erl
+++ system1/process.erl
@@ -11,36 +11,30 @@ next(Ns, Id) ->
   receive
     {task1, start, Max_Messages, Timeout} ->
       timer:send_after(Timeout, stop),
-      Map = [{N, {0, 0}} || N <- Ns],
+      Map = maps:from_list([{N, 0} || N <- Ns]),
       self() ! broadcast,
       if Max_Messages == 0 ->
-        task1(Map, infinity, Id);
+        task1(Map, infinity, Id, 0);
       true ->
-        task1(Map, Max_Messages, Id)
+        task1(Map, Max_Messages, Id, 0)
       end
   end.
 
-task1(Ns, Max_Messages, Id) ->
+task1(Map, Max_Messages, Id, Sen) ->
   receive
     {hello, Sender} ->
-      NextNs = update_recs(Sender, Ns),
-      task1(NextNs, Max_Messages, Id);
-    broadcast ->  
-      [N ! {hello, self()} || {N, {_, Sen}} <- Ns, Sen < Max_Messages],
-      WithSends = [add_sends(N, Max_Messages) || N <- Ns],
-      task1(WithSends, Max_Messages, Id);
-    stop -> io:format("~p: ~w~n", [Id, lists:map(fun({_, Snd}) -> Snd end, Ns)])
-  after 0 -> self() ! broadcast, task1(Ns, Max_Messages, Id)
+      NewMap = Map#{Sender := maps:get(Sender, Map) + 1},
+      task1(NewMap, Max_Messages, Id, Sen);
+    stop ->
+      List = maps:to_list(Map),
+      WithSen = lists:map(fun({_, Rec}) -> {Sen, Rec} end, List),
+      io:format("~p: ~w~n", [Id, WithSen])
+  after 0 -> 
+    if Sen < Max_Messages ->  
+      [N ! {hello, self()} || {N, _} <- maps:to_list(Map)],
+      task1(Map, Max_Messages, Id, Sen + 1);
+    true ->
+      task1(Map, Max_Messages, Id, Sen)
+    end
   end.
 
-add_sends({N, {Rec, Sen}}, Max_Messages) ->
-  if Sen < Max_Messages ->
-    {N, {Rec, Sen + 1}};
-  true ->
-    {N, {Rec, Sen}}
-  end.
-
-update_recs(Sender, [{Sender, {Rec, Sen}} | Rest]) ->
-  [{Sender, {Rec + 1, Sen}} | Rest];
-update_recs(Sender, [Head | Rest]) ->
-  [Head | update_recs(Sender, Rest)].
